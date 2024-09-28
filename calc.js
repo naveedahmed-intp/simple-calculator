86a01367431dcd6e3366cf19fec8c2408054b134
naveedahmed-intp <dc230400318nak@vu.edu.pk>
2024-09-28 20:14:58 +0500

    Add, subtract, multiply and divide with little bugs still to remove

--- calc.js
+++ calc.js
@@ -1,7 +1,12 @@
 let container = document.querySelector("#buttons");
-let btnArray = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "x", "=", "AC", "0", "/"];
+let idArray = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "x", "=", "AC", "0", "/"];
 let classArray = ["n", "n", "n", "add", "n", "n", "n", "minus", "n", "n", "n", "multiply", "eq", "ac", "n", "dvd"];
 let display = document.querySelector("#display");
+const audio = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/clickUp.mp3");
+
+function playSound() {
+    audio.play();
+}
 
 window.addEventListener('load', () => {
     let defaultGridSize = 4;
@@ -12,33 +17,72 @@ function makeGrid(size) {
         for(let i = 0; i < Math.pow(size, 2); i++) {
             let btn = document.createElement("button");
             btn.setAttribute("class", `button ${classArray[i]}`);
-            btn.id = btnArray[i];
+            btn.id = idArray[i];
             btn.style.padding = 20 + "px";
             btn.style.backgroundColor = "white";
-            btn.textContent = btnArray[i];
+            btn.textContent = idArray[i];
             container.appendChild(btn);
         }
     }
 }
-let number = "", res = 0, temp = 0;
+let firstNumber = "", secondNumber = "", res = 0, operator = "";
 
 container.addEventListener('click', (e) => {
-    if(e.target.classList.contains("n")) {
-        number += e.target.id;
-        display.textContent = number;
-    }
+        playSound();
+        if (operator == "") {
+            if(e.target.classList.contains("n")) {
+                firstNumber += e.target.id;
+                display.textContent = firstNumber;
+            }
+        }
+        if((e.target.classList.contains("add") || e.target.classList.contains("minus") || e.target.classList.contains("multiply") || e.target.classList.contains("dvd")) && (secondNumber == "" || firstNumber == res)) {
+            operator = e.target.id;
+            display.textContent = firstNumber + " " + operator;
+        }
+        if((operator == "+" || operator == "-" || operator == "x" || operator == "/") && e.target.classList.contains("n")) {
+                secondNumber += e.target.id;
+                display.textContent = firstNumber + " " + operator + " " + secondNumber;
+            }
 
-    if(e.target.classList.contains("ac")) {
-        display.textContent = "";
-        number = "";
+        if(e.target.classList.contains("eq")) {
+            switch(operator) {
+                case '+':
+                res = parseInt(firstNumber) + parseInt(secondNumber);
+                display.textContent = firstNumber + " " + operator + " " + secondNumber + " = " + res;
+                firstNumber = res;
+                secondNumber = "";
+                break;
+                case '-':
+                res = parseInt(firstNumber) - parseInt(secondNumber);
+                display.textContent = firstNumber + " " + operator + " " + secondNumber + " = " + res;
+                firstNumber = res;
+                secondNumber = "";
+                break;
+                case 'x':
+                res = parseInt(firstNumber) * parseInt(secondNumber);
+                display.textContent = firstNumber + " " + operator + " " + secondNumber + " = " + res;
+                firstNumber = res;
+                secondNumber = "";
+                break;
+                case '/':
+                res = Math.round(parseInt(firstNumber) / parseInt(secondNumber)*100)/100;
+                display.textContent = firstNumber + " " + operator + " " + secondNumber + " = " + res;
+                firstNumber = res;
+                secondNumber = "";
+                break;
+            }
+        }
+
+        if(e.target.classList.contains("ac")) {
+            display.textContent = "";
+            reset();
+        }
+        
+    });
+
+    function reset() {
+        firstNumber = "";
+        secondNumber = "";
+        operator = "";
         res = 0;
-        temp = 0;
     }
-
-    if(e.target.classList.contains("add")) {
-        temp = parseInt(number);
-        res += temp;
-        number = "";
-        display.textContent = res;
-    }
-})
